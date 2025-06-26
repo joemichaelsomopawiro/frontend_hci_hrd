0544161ab995065b1d92a58396da717483c6b558
Albert JH <2281053@unai.edu>
2025-06-26 16:12:51 +0700

    perbaiki logo notifikasi

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -236,7 +236,13 @@
             </button>
             
             <button v-if="canViewNotifications" class="notification-btn" @click="toggleNotificationDropdown" ref="notificationBtn">
-              <span class="notification-icon">🔔</span>
+              <span class="notification-icon">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
+                  <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
+                  <circle cx="18" cy="6" r="3" fill="currentColor" opacity="0.8"/>
+                </svg>
+              </span>
               <span v-if="notificationCount > 0" class="notification-badge">{{ notificationCount }}</span>
               
               <!-- Notification Dropdown -->
